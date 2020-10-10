7b0e0f8935982491a43e6a754de631267e39081c
EC2 Default User <ec2-user@ip-172-31-11-24.ec2.internal>
2020-10-10 12:15:07 +0000

    応用課題４です。よろしくお願い致します。

--- app/models/book_coomment.rb
+++
@@ -1,5 +0,0 @@
-class BookCoomment < ApplicationRecord
-    belongs_to :user
-    belongs_to :book
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
+  get 'users/index'
   root 'home#index'
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  resources :users, only: [:index, :create, :show, :edit, :update]
+  resources :users, only: [:index, :create, :show, :edit, :update] do
+    get 'follows' => 'relationships#follows', as: 'follows'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   end
